4dd984a3ffad8b91bd2da9b196067bd23f0f4434
Anirudh S <anirudh6626@gmail.com>
2020-07-04 09:47:24 +0530

    updated tests

--- test/irmin-containers/lww_register.ml
+++ test/irmin-containers/lww_register.ml
@@ -14,7 +14,7 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
- 
+
 open Lwt.Infix
 
 module In : Irmin_containers.Lww_register.Input with type t = int = struct
