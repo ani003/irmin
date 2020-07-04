0be8e49d8b7707df2ad1468c1d79d02631a5b0d3
Anirudh S <anirudh6626@gmail.com>
2020-07-04 09:45:05 +0530

    log test added

--- test/irmin-containers/test.ml
+++ test/irmin-containers/test.ml
@@ -1,3 +1,5 @@
 open Lwt.Infix
 
-let _ = Lwt_main.run (Counter.test () >>= Lww_register.test >>= Blob_log.test)
+let _ =
+  Lwt_main.run
+    (Counter.test () >>= Lww_register.test >>= Blob_log.test >>= Log.test)
